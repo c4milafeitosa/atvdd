139121bc0db5d7b304f76cdcfa46f9a8e55c5dfd
c4milafeitosa <110909278+c4milafeitosa@users.noreply.github.com>
2024-10-28 20:02:09 -0300

    Create fundamentos-questao02.js

---
+++ fundamentos-questao02.js
@@ -0,0 +1,9 @@
+// Definindo a base e a altura do triângulo
+const base = 10; // Substitua pelo valor desejado
+const altura = 5; // Substitua pelo valor desejado
+
+// Calculando a área do triângulo
+const area = (base * altura) / 2;
+
+// Exibindo o resultado
+console.log(`A área do triângulo é: ${area}`);
